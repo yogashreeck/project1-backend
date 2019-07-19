af205ceeb75287469513f883358647be5ca5605a
Yogashreeck <ykumarostb1@gmail.com>
2019-07-19 22:45:30 +0530

    register and login backend completed

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+var app = express()
+var mongoose = require("mongoose")
+var port = process.env.PORT || 8000
+// mongoose.set('useCreateIndex', true);
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/project1-backend',{ useNewUrlParser: true })
+// .then(
+//     () => {
+//       console.log('Connected to mongoDB');
+//     },
+//     (err) => console.log('Error connecting to mongoDB', err)
+//   );
+
+const mongoURI = 'mongodb://localhost:27017/project1-backend'
+
+mongoose
+.connect(mongoURI, {useNewUrlParser: true})
+.then(() => console.log("mongoDB connected"))
+.catch(err => console.log(err))
+
+var Users = require('./routes/Users')
+
+app.use('/users', Users)
+
+app.get('/', function(req, res) {
+  res.send('hello');
+});
+
+
+app.listen(port,() => {
+    console.log("server is running on port :" + port)
+})
