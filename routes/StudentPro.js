2a9480fa8604a8dcf49b64a920f26ca1fb229272
yogashreeck <ykumarostb1@gmail.com>
2019-07-25 16:59:11 +0530

    student profile added

--- routes/StudentPro.js
+++ routes/StudentPro.js
@@ -1,13 +1,63 @@
-const express = require('express')
+const express = require("express")
+const profile = express.Router();
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
-const MovieCtrl = require('../controllers/Student-Crtl')
+const StudentProfile = require("../models/StudentProfile")
+profile.use(cors())
 
-const router = express.Router()
+// process.env.SECRET_KEY - 'secret'
 
-router.post('/movie', MovieCtrl.createMovie)
-router.put('/movie/:id', MovieCtrl.updateMovie)
-router.delete('/movie/:id', MovieCtrl.deleteMovie)
-router.get('/movie/:id', MovieCtrl.getMovieById)
-router.get('/movies', MovieCtrl.getMovies)
+profile.post('/studentProfile', (req,res) =>{
+    const userData = {
+        studentname: req.body.studentname,
+        address: req.body. address,
+        email: req.body.email,
+        mobileNumber: req.body.mobileNumber
+    }
+    StudentProfile.findOne({
+        studentname: req.body.studentname,
+        address: req.body. address,
+        email: req.body.email,
+        mobileNumber: req.body.mobileNumber
+    })
+    .then(user => {
+        if(!user){
+            bcrypt.hash(req.body.studentname, 10, (err,hash) => {
+                StudentProfile.create(userData)
+                .then(user => {
+                    res.json({status: 'student registered!'})
+                })
+                .catch(err => {
+                    res.send('error: ' + err)
+                })
+            })
+        }else{
+            res.json({error: 'User already exists'})
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+})
+profile.get('/studentProfile', (req, res) => {
+//    const decoded = jwt.verify(req.header['authorization'], process.env.SECRET_KEY)
 
-module.exports = router
+    StudentProfile.find({
+        // _id: decoded._id
+    })
+    .then(user => {
+        if(user) {
+            res.json(user)
+        }else{
+            res.send("User does not exist")
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+})
+
+
+module.exports = profile; 
